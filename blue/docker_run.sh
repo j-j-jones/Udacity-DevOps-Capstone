59aa41f1821f798d6c0bfd3662fe950e6d9684f5
J. Jonathan (J.J.) Jones <jj.jones@live.com>
2019-09-11 19:36:34 -0400

    Update docker_run.sh

--- blue/docker_run.sh
+++ blue/docker_run.sh
@@ -2,12 +2,12 @@
 
 # STEP-01:
 # BUILD DOCKER IMAGE WITH DESCRIPTIVE TAG
-docker build --tag=blueImage .
+sudo docker build --tag=blueImage .
 
 # STEP-02:
 # LIST DOCKER IMAGES
-docker image ls
+sudo docker image ls
 
 # STEP-03:
 # RUN DOCKER IMAGE
-docker run -p 8000:80 blueImage
+sudo docker run -p 8000:80 blueImage
